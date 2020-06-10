005f042d2a6b1f5a18b3eba4b866e04038eb8150
jDan735 <daniel734@bk.ru>
2020-06-11 01:48:56 +0300

    v 0.2.0

--- kobra/core.py
+++ kobra/core.py
@@ -1,26 +1,58 @@
 from kobra import __version__
 import argparse
+import os
 
-class Kobra:
-    def sendVersion():
-        print("Kobra v" + __version__)
+def getModuleName(filelist):
+    folders = []
+    for file in filelist:
+        if os.path.isdir(file):
+            folders.append(file)
+
+    for folder in folders:
+        files = os.listdir(folder)
+        for file in files:
+            if file == "__init__.py":
+                return folder
 
 def kobra():
 
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("command", nargs="?", default="install", help="Run command (deploy, install, develop)")
     parser.add_argument("-v", "--version", action="store_true", help="Show version")
-    # parser.add_argument("-d", "--deploy", help="Deploy")
-    parser.add_argument("-n", "--name", help="Enter module name")
 
     args = parser.parse_args()
 
     if args.version:
-        Kobra.sendVersion()
+        print("Kobra v" + __version__)
         quit()
 
-    if args.name == None:
-        print("Enter name!")
-        quit()
+    if args.command == "deploy":
+        cwd = os.getcwd()
+        moduleName = getModuleName(os.listdir("."))
+        if moduleName == None:
+            print("Program can't find module folder")
+            quit()
+        else:
+            with open(os.getcwd() + "\\" + moduleName + "\\" + "__init__.py") as init:
+                moduleVersion = (init.read()
+                    .replace("__version__", "")
+                    .replace(" ", "")
+                    .replace("\"", "")
+                    .replace("=", ""))
+
+            version = input("Enter version (current: " + moduleVersion + ") ")
+            with open(os.getcwd() + "\\" + moduleName + "\\__init__.py", "w") as init:
+                init.write("__version__ = \"" + version + "\"")
+            if os.path.exists("dist"):
+                for file in os.listdir("dist"):
+                    os.remove("dist" + "\\" + file)
+            os.system("python setup.py sdist")
+            os.system("twine upload dist\\*")
+        print()
+    elif args.command == "install":
+        os.system("python setup.py install")
+    elif args.command == "develop":
+        os.system("python setup.py develop")
     else:
-        print("Module name: " + args.name)
+        print("Enter command!")
